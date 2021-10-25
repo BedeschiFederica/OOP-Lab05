04dd68f7b2dcf325daa0f6366422fd6ea1241a16
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2021-10-25 11:17:17 +0200

    New class NewStringComparator

---
+++ lab05/src/it/unibo/oop/lab05/ex2/NewStringComparator.java
@@ -0,0 +1,10 @@
+package it.unibo.oop.lab05.ex2;
+
+import java.util.Comparator;
+
+public class NewStringComparator implements Comparator<String> {
+
+	public int compare(String o1, String o2) {
+		return Integer.parseInt(o1) - Integer.parseInt(o2);
+	}
+}
