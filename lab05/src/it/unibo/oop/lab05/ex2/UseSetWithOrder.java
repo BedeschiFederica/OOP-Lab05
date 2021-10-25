6c32fbb87ca4467a86db420af9bd2d88169874b2
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2021-10-25 11:17:46 +0200

    Create main code in UseSetWithOrder

--- lab05/src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
+++ lab05/src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
@@ -1,5 +1,8 @@
 package it.unibo.oop.lab05.ex2;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * 
  */
@@ -24,5 +27,11 @@ public final class UseSetWithOrder {
          * 
          * 3) Prints the set, which must be ordered
          */
+    	
+    	final Set<String> set = new TreeSet<>(new NewStringComparator());
+    	for (int i = 0; i < 100; i++) {
+    		set.add("" + (int)(Math.random() * 100));
+    	}
+    	System.out.println(set);
     }
 }
